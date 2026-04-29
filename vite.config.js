65d08c9e254bf8ad256105b0ef8bc1563e3bd791
Muhammad Ashhar Alam <m.ashhar10@gmail.com>
2026-04-30 01:21:15 +0500

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   base: './', // This ensures all asset paths are relative (./assets/...)
+  plugins: [viteSingleFile()],
   build: {
     outDir: 'dist',
   }
